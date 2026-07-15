7317d19ca1a1c810d3231d1df05c47dc5375ba01
Sheep26 <drew@drewfitzgerald.co.nz>
2026-07-15 15:08:21 +1200

    That's better

--- firmware/main/exercise.c
+++ firmware/main/exercise.c
@@ -1,24 +1,11 @@
 #include <exercise.h>
 
 void addExercisePoint(exercise_t *exercise, exercise_point_t *point) {
-    if (!exercise->list) {
+    if (!exercise->list)
         exercise->list = point;
-        exercise->last = point;
-
-        return;
-    }
 
     if (exercise->last)
         exercise->last->next = point;
-    else {
-        // Fallback incase exercise->last is not set yet.
-        exercise_point_t *p = exercise->list;
-
-        while (p->next)
-            p = p->next;
-
-        p->next = point;
-    }
 
     exercise->last = point;
 }
