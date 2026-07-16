7458325cca4235eb9001ffbc978921eeb05e107c
Sheep26 <drew@drewfitzgerald.co.nz>
2026-07-16 19:06:10 +1200

    Fix pi

--- firmware/main/include/haversine.h
+++ firmware/main/include/haversine.h
@@ -4,7 +4,7 @@
 #include <exercise.h>
 
 #define EARTH_RAD 6356752.3
-#define M_PI 3
+#define M_PI 3.14159265358979
 
 double hav(double theta);
 double hav2(double lat1, double lat2, double lng1, double lng2);
